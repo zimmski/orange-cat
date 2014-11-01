e998ef5e8c8865102e63d4c74fe3d97a581e0221
Hyunje Alex Jun <me@noraesae.net>
2014-11-01 23:04:06 +0000

    Rewrite CLI with codegangsta/cli.
    Now it's cool enough.

--- main.go
+++ main.go
@@ -1,44 +1,57 @@
 package main
 
 import (
-	"flag"
-	"fmt"
+	"github.com/codegangsta/cli"
+	"os"
 )
 
 func main() {
-	basic := flag.Bool("basic", false, "Use Markdown Basic(Markdown Common by default).")
-	help := flag.Bool("help", false, "Print usage message.")
-	port := flag.Int("port", 6060, "Port to listen.")
-	flag.Parse()
+	app := cli.NewApp()
+	app.Name = "orange"
+	app.Usage = `orange-cat is a Markdown previewer written in Go.
+   Its main goal is to be used with any editor you love.
+   For information, please visit https://github.com/noraesae/orange-cat`
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:  "basic, b",
+			Usage: "Use Markdown Basic(Markdown Common by default).",
+		},
+		cli.IntFlag{
+			Name:  "port, p",
+			Value: 6060,
+			Usage: "Port to listen.",
+		},
+	}
+	app.Action = func(c *cli.Context) {
+		args := c.Args()
+		if len(args) != 1 {
+			cli.ShowAppHelp(c)
+			return
+		}
 
-	if *help {
-		printHelp()
-		return
+		orange := NewOrange(args[0])
+
+		if c.Bool("basic") {
+			orange.UseBasic()
+		}
+
+		orange.Run(c.Int("port"))
 	}
 
-	args := flag.Args()
-	if len(args) != 1 {
-		printError("No file provided. Please check the usage.\n")
-		printHelp()
-		return
-	}
+	// codegangsta/cli help template
+	cli.AppHelpTemplate = `orange-cat
+   {{.Usage}}
 
-	orange := NewOrange(args[0])
+USAGE:
+   {{.Name}} [global options] [command] file
 
-	if *basic {
-		orange.UseBasic()
-	}
+COMMANDS:
+   {{range .Commands}}{{.Name}}{{with .ShortName}}, {{.}}{{end}}{{ "\t" }}{{.Usage}}
+   {{end}}{{if .Flags}}
+GLOBAL OPTIONS:
+   {{range .Flags}}{{.}}
+   {{end}}{{end}}
+`
 
-	orange.Run(*port)
-}
-
-func printError(message string) {
-	fmt.Printf("\x1b[31m%s\x1b[39;49m\n", message)
-}
-
-func printHelp() {
-	fmt.Println("Usage:")
-	fmt.Println("  orange [options] file\n")
-	fmt.Println("Options:")
-	flag.PrintDefaults()
+	app.Run(os.Args)
 }
