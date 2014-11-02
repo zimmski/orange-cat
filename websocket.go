be614c983370335a8a2cf5850a2457cb8f4cd481
Hyunje Alex Jun <me@noraesae.net>
2014-11-02 22:46:23 +0000

    Add websocket functionality.
    Now it works basically.

--- websocket.go
+++ websocket.go
@@ -1,8 +1,21 @@
 package main
 
 import (
+	"fmt"
+	goWs "github.com/gorilla/websocket"
 	"net/http"
+	"time"
 )
+
+const (
+	WriteTimeout = 5 * time.Second
+	BufferSize   = 2048
+)
+
+var upgrader = goWs.Upgrader{
+	ReadBufferSize:  BufferSize,
+	WriteBufferSize: BufferSize,
+}
 
 type Websocket struct {
 	markdown <-chan *string
@@ -12,5 +25,48 @@ func NewWebsocket(markdown <-chan *string) *Websocket {
 	return &Websocket{markdown}
 }
 
+func (ws *Websocket) Reader(c *goWs.Conn, closed chan<- bool) {
+	defer c.Close()
+	for {
+		messageType, _, err := c.NextReader()
+		if err != nil || messageType == goWs.CloseMessage {
+			break
+		}
+	}
+	closed <- true
+}
+
+func (ws *Websocket) Writer(c *goWs.Conn, closed <-chan bool) {
+	defer c.Close()
+	for {
+		select {
+		case data := <-ws.markdown:
+			c.SetWriteDeadline(time.Now().Add(WriteTimeout))
+			// FIXME: Markdown convert
+			err := c.WriteMessage(goWs.TextMessage, []byte(*data))
+			if err != nil {
+				return
+			}
+		case <-closed:
+			return
+		}
+	}
+}
+
 func (ws *Websocket) Serve(w *http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(*w, "Method not allowed", 405)
+		return
+	}
+
+	sock, err := upgrader.Upgrade(*w, r, nil)
+	if err != nil {
+		fmt.Println("Can't connect to websocket")
+		return
+	}
+
+	closed := make(chan bool)
+
+	go ws.Reader(sock, closed)
+	ws.Writer(sock, closed)
 }
