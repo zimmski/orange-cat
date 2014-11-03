f414f7321a4a4980bb74c113aea83c5144dad852
Hyunje Alex Jun <me@noraesae.net>
2014-11-03 16:28:14 +0000

    Implement Markdown converter.

--- md_chan.go
+++ md_chan.go
@@ -1,13 +1,36 @@
 package main
 
+import (
+	"github.com/russross/blackfriday"
+)
+
 type MdChan struct {
 	data    chan *string
 	request chan bool
 }
 
-func NewMdChan(watcherDataChan *DataChan, useBasic bool) *MdChan {
-	// FIXME: implement Markdown Converter
-	dataChan := watcherDataChan.data
+func (md *MdChan) MarkdownConverter(rawDataChan chan *string, useBasic bool) {
+	var convert func([]byte) []byte
+	if useBasic {
+		convert = blackfriday.MarkdownBasic
+	} else {
+		convert = blackfriday.MarkdownCommon
+	}
 
-	return &MdChan{dataChan, watcherDataChan.request}
+	for {
+		select {
+		case rawData := <-rawDataChan:
+			data := string(convert([]byte(*rawData)))
+			md.data <- &data
+		default:
+		}
+	}
+}
+
+func NewMdChan(watcherDataChan *DataChan, useBasic bool) *MdChan {
+	mdChan := MdChan{make(chan *string), watcherDataChan.request}
+
+	go mdChan.MarkdownConverter(watcherDataChan.data, useBasic)
+
+	return &mdChan
 }
