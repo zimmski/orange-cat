6ddbba5cc549375152212f616ab00b06d9fd2268
Hyunje Alex Jun <me@noraesae.net>
2014-11-06 18:49:25 +0000

    Use a custom CSS if it exists.
    The CSS is loaded from `$HOME/.orange-cat.css`.

--- tests/template_test.go
+++ tests/template_test.go
@@ -7,6 +7,9 @@ import (
 	. "github.com/onsi/gomega"
 
 	"net/http"
+	"os"
+	"os/user"
+	"path/filepath"
 	"reflect"
 	"strings"
 )
@@ -29,6 +32,35 @@ var _ = Describe("Template", func() {
 			Expect(strings.Contains(w.data, "var conn = new WebSocket(\"ws://localhost:1234/ws\");")).
 				To(Equal(true))
 		})
+
+		It("uses a custom CSS if it exists.", func() {
+			if CustomCSSPath() == "" {
+				// If there's no custom css path, just skip this test.
+				return
+			}
+
+			// Without a custom CSS
+			notExist := os.Rename(CustomCSSPath(), CustomCSSPath()+"_")
+			template := Template("temp_file.md", 1234)
+			w := TestResponseWriter{}
+			template(&w)
+			Expect(strings.Contains(w.data, "<style>")).To(Equal(true))
+
+			// With a custom CSS
+			os.Create(CustomCSSPath())
+			template = Template("temp_file.md", 1234)
+			w = TestResponseWriter{}
+			template(&w)
+			customCSS, _ := CustomCSS()
+			Expect(strings.Contains(w.data, *customCSS)).To(Equal(true))
+
+			// Reset the original file or remove test file.
+			if notExist != nil {
+				os.Remove(CustomCSSPath())
+			} else {
+				os.Rename(CustomCSSPath()+"_", CustomCSSPath())
+			}
+		})
 	})
 })
 
@@ -42,3 +74,11 @@ func (w *TestResponseWriter) Write(data []byte) (int, error) {
 	return len(data), nil
 }
 func (w *TestResponseWriter) WriteHeader(int) {}
+
+func CustomCSSPath() string {
+	usr, err := user.Current()
+	if err != nil {
+		return ""
+	}
+	return filepath.Join(usr.HomeDir, ".orange-cat.css")
+}
